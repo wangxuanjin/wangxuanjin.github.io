6ef807952736468a5a837a41ee64d78f3254a480
wangxuanjin <1935659801@qq.com>
2023-11-12 23:34:29 +0800

    日常demo练习

---
+++ 第四章/22_Module 的语法/ES6/index.js
@@ -0,0 +1,12 @@
+// import { Hello, firstName,getName as getMyName} from "./hello.js"
+
+// function getName(){
+//     console.log("hhh");
+// }
+
+// getName();
+// getMyName();
+
+
+import CustomName from "./hello.js"
+CustomName();
